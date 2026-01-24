e27951dc6339067280495f64644759338c9ff192
addy5566 <aditya82533@gmail.com>
2026-01-24 19:50:52 +0530

    added test case settings

--- edutech/settings.py
+++ edutech/settings.py
@@ -101,6 +101,15 @@ else:
         }
     }
 
+import sys
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
 
 
 
